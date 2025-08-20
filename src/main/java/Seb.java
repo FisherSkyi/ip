e95fee482ea3117d15d4f96da26b18043af09d96
YuLetian <yu.letian66@gmail.com>
2025-08-20 17:36:12 +0800

    Add add & list

--- src/main/java/Seb.java
+++ src/main/java/Seb.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Seb {
     private final String name = "Seb";
+    private static ArrayList<String> things = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner((System.in));
+        things.clear();
         System.out.println(
                 "    ____________________________________________________________\n" +
-                "     Hello! I'm [YOUR CHATBOT NAME]\n" +
+                "     Hello! I'm Seb\n" +
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________"
         );
@@ -23,8 +26,18 @@ public class Seb {
                         "    ____________________________________________________________");
                 break;
             }
-            if (!line.isEmpty()) {
-                System.out.println(line);
+            if (line.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < things.size(); i++) {
+                    System.out.println(String.format("     %d. %s",i, things.get(i)));
+                }
+                System.out.println("    ____________________________________________________________");
+            } else if (!line.isEmpty()) {
+                things.add(line);
+                System.out.println(
+                        "    ____________________________________________________________\n" +
+                        String.format("     added: %s\n", line) +
+                        "    ____________________________________________________________");
             }
         }
     }
