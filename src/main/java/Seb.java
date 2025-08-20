835e57835960e7a7306f89b3946d124b58bcd08f
YuLetian <yu.letian66@gmail.com>
2025-08-20 17:19:53 +0800

    Add echo feature

--- src/main/java/Seb.java
+++ src/main/java/Seb.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Seb {
     private final String name = "Seb";
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner((System.in));
         System.out.println(
-                "____________________________________________________________\n" +
-                        " Hello! I'm Seb\n" +
-                        " What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        " Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n"
+                "    ____________________________________________________________\n" +
+                "     Hello! I'm [YOUR CHATBOT NAME]\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________"
         );
+        while (true) {
+            if (!sc.hasNextLine()) {
+                break;
+            }
+            String line = sc.nextLine().trim();
+            if (line.equals("bye")) {
+                System.out.println(
+                        "    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________");
+                break;
+            }
+            if (!line.isEmpty()) {
+                System.out.println(line);
+            }
+        }
     }
 }
