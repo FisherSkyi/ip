73df5e4da8326876523c9ad34493db3eceeea2b5
YuLetian <yu.letian66@gmail.com>
2025-08-20 14:50:36 +0800

    Add greet and exit feature

---
+++ src/main/java/Seb.java
@@ -0,0 +1,14 @@
+public class Seb {
+    private final String name = "Seb";
+
+    public static void main(String[] args) {
+        System.out.println(
+                "____________________________________________________________\n" +
+                        " Hello! I'm Seb\n" +
+                        " What can I do for you?\n" +
+                        "____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n"
+        );
+    }
+}
