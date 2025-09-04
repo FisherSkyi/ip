3ef07468def24d96ab4128bcb93b42ed3cf8a499
YuLetian <yu.letian66@gmail.com>
2025-09-04 11:35:30 +0800

    Check style

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -6,27 +6,21 @@ import java.util.ArrayList;
  */
 public class TaskList {
     private ArrayList<Task> tasks;
-    
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
-    
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
-    
     public Task getTasks(int index) {
         return this.tasks.get(index);
     }
-    
     public void deleteTasks(int index) {
         this.tasks.remove(index);
     }
-    
     public void addTasks(Task t) {
         this.tasks.add(t);
     }
-    
     public int size() {
         return this.tasks.size();
     }
--- src/main/java/duke/WrongDescriptionException.java
+++ src/main/java/duke/WrongDescriptionException.java
@@ -1,9 +1,19 @@
 package duke;
+
+/**
+ * Exception thrown when the description of a task is not correct.
+ */
 public class WrongDescriptionException extends Exception {
+    /**
+     * Constructor for WrongDescriptionException.
+     * @param taskType The type of task with the wrong description.
+     */
     public WrongDescriptionException(String taskType) {
-        super("     OOPS!!! The description of a " + taskType + " is not correct.\n" +
-              "     | for todo,     use todo     (description)\n" +
-              "     | for deadline, use deadline (description) /by   (time)\n" +
-              "     | for event,    use event    (description) /from (time) /to (time)");
+        super("     OOPS!!! The description of a "
+                + taskType
+                + " is not correct.\n"
+                + "     | for todo,     use todo     (description)\n"
+                + "     | for deadline, use deadline (description) /by   (time)\n"
+                + "     | for event,    use event    (description) /from (time) /to (time)");
     }
 }
