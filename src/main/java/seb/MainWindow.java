392dbbcce472926feb6c980403abb36ed72a38bc
YuLetian <yu.letian66@gmail.com>
2025-09-05 01:29:43 +0800

    Correct bye command

--- src/main/java/seb/MainWindow.java
+++ src/main/java/seb/MainWindow.java
@@ -1,5 +1,6 @@
 package seb;
 
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -7,6 +8,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
 /**
  * Controller for the main GUI.
  */
@@ -23,8 +27,7 @@ public class MainWindow extends AnchorPane {
     private Seb seb;
     
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.jpg"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaSeb.png"));
-    
+    private Image sebImage = new Image(this.getClass().getResourceAsStream("/images/DaSeb.png"));
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
@@ -40,14 +43,30 @@ public class MainWindow extends AnchorPane {
      * the dialog container. Clears the user input after processing.
      */
     @FXML
-    private void handleUserInput() throws UnknownInputException, WrongDescriptionException {
-        String input = userInput.getText();
-        String response = seb.getResponse(input);
-        dialogContainer.getChildren().addAll(
-                DialogBox.getUserDialog(input, userImage),
-                DialogBox.getDukeDialog(response, dukeImage)
-        );
-        userInput.clear();
+    public void handleUserInput() throws UnknownInputException, WrongDescriptionException {
+        String userText = userInput.getText();
+        if (!userText.trim().equalsIgnoreCase("bye")) {
+            String sebText = seb.getResponse(userInput.getText());
+            dialogContainer.getChildren().addAll(
+                    DialogBox.getUserDialog(userText, userImage),
+                    DialogBox.getDukeDialog(sebText, sebImage)
+            );
+            userInput.clear();
+        } else {
+            dialogContainer.getChildren().add(
+                    DialogBox.getDukeDialog("Bye! See you soon.", sebImage)
+            );
+            userInput.setDisable(true);
+            sendButton.setDisable(true);
+            PauseTransition delay = new PauseTransition(Duration.millis(400));
+            delay.setOnFinished(e -> {
+                Stage stage = (Stage) userInput.getScene().getWindow();
+                stage.close();
+                javafx.application.Platform.exit(); // shut down JavaFX runtime
+            });
+            
+            delay.play();
+        }
     }
 }
 
