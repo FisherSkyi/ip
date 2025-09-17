5eb78cea7ecf643f837b1b9cb902fb4297b4c971
YuLetian <yu.letian66@gmail.com>
2025-09-17 23:12:52 +0800

    GUI update

--- src/main/java/seb/DialogBox.java
+++ src/main/java/seb/DialogBox.java
@@ -13,6 +13,11 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.effect.DropShadow;
+
 /**
  * This control represents a dialog box consisting of an ImageView to represent the speaker's face and a label
  * containing text from the speaker.
@@ -22,6 +27,8 @@ public class DialogBox extends HBox {
     private Label dialog;
     @FXML
     private ImageView displayPicture;
+    @FXML
+    private StackPane profilePictureContainer;
     /**
      * Constructor for DialogBox.
      * @param text The text to be displayed in the dialog box.
@@ -38,6 +45,20 @@ public class DialogBox extends HBox {
         }
         dialog.setText(text);
         displayPicture.setImage(img);
+        // Make the profile picture circular
+        Circle circleClip = new Circle(
+            displayPicture.getFitWidth() / 2,
+            displayPicture.getFitHeight() / 2,
+            Math.min(displayPicture.getFitWidth(), displayPicture.getFitHeight()) / 2
+        );
+        displayPicture.setClip(circleClip);
+        // Add shadow effect to the profile picture container
+        DropShadow shadow = new DropShadow();
+        shadow.setRadius(10);
+        shadow.setOffsetX(5);
+        shadow.setOffsetY(5);
+        shadow.setColor(Color.rgb(0, 0, 0, 0.2));
+        profilePictureContainer.setEffect(shadow);
     }
     private void flip() {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
